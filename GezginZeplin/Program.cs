8c78b1de63e24db283e32a3e4e5a2105b977781d
Tarik Bir <7569835+tarikbir@users.noreply.github.com>
2018-02-28 01:25:07 +0300

    Bu gece son

--- GezginZeplin/Program.cs
+++ GezginZeplin/Program.cs
@@ -11,18 +11,46 @@ namespace GezginZeplin
     {
         static void Main(string[] args)
         {
-            StreamReader adjacent = new StreamReader("adjacent.txt");
-            StreamReader cities = new StreamReader("cities.txt");
-            cities.ReadLine();
+            //Initialization
+            string citiesFileName = "cities.txt";
+            string adjacentFileName = "adjacent.txt";
+            City[] cityArray = new City[81];
 
-            while (!cities.EndOfStream)
+            //File checks
+            if (!File.Exists(citiesFileName))
             {
-                string line = cities.ReadLine();
-                double[] tempString = Array.ConvertAll(line.Split(','), Double.Parse);
-                City c = new City(tempString[0]/10000,tempString[1]/10000,(int)tempString[2],(int)tempString[3]);
-                c.writeCity(); //DEBUG
+                Console.WriteLine("Cities file not found... Press any key to exit.");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            if (!File.Exists(adjacentFileName))
+            {
+                Console.WriteLine("Adjacent file not found... Press any key to exit.");
+                Console.ReadKey();
+                Environment.Exit(0);
             }
 
+            //File reading: Cities
+            using (StreamReader cities = new StreamReader(citiesFileName))
+            {
+                cities.ReadLine(); //Pass the first line
+                int i = 0;
+                while (!cities.EndOfStream)
+                {
+                    string line = cities.ReadLine();
+                    double[] tempString = Array.ConvertAll(line.Split(','), Double.Parse);
+                    cityArray[i++] = new City(tempString[0] / 10000, tempString[1] / 10000, (int)tempString[2], (int)tempString[3]);
+                    cityArray[i-1].writeCity(); //DEBUG, DELETE LATER
+                }
+            }
+
+            //File reading, connecting nodes
+            using (StreamReader adjacent = new StreamReader(adjacentFileName))
+            {
+
+            }
+
+            //END OF PROGRAM
             Console.Read();
         }
     }
